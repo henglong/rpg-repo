5c4cb75bdd8e1cfad00a69cb96b939b83e7f2f77
henglong <tenghenglong@gmail.com>
2016-09-28 18:57:31 +0000

    0.0.1.10

--- database/migrations/2016_09_28_183654_create_games_table.php
+++ database/migrations/2016_09_28_183654_create_games_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGamesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('games', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->interger('game_id')->unsigned();
-            $table->rememberToken();
+            $table->interger('User_id')->unsigned()->index();
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('games');
     }
 }
