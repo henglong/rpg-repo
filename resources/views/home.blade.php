74c5acc944f6b4494d5639d16627273b8d6316a5
henglong <tenghenglong@gmail.com>
2016-10-01 02:50:00 +0000

    0.0.1.13

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,67 +2,11 @@
 
 @section('header')
     <title>RPG - Game APP</title>
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
 @stop
 
 @section('content')
 
-        <div class="flex-center position-ref full-height">
+        <div class="container">
             @if (Route::has('login'))
                 <div class="top-right links">
                     <a href="{{ url('/login') }}">Login</a>
@@ -70,11 +14,10 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
+            <div class="jumbotron">
+                <h1>
                     Welcome
-                </div>
-
+                </h1>
             </div>
         </div>
 
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,8 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/app.css" type="text/css" />
         
         @yield('header')
+        
     </head>
     <body>
         
